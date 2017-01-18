37f99c34634789581789c40879e39aee8c01fa03
chenshuangyu <695166116@qq.com>
2017-01-18 22:10:35 +0800

    添加项目文件。

---
+++ 远程控制/AppConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 远程控制
+{
+    public class AppConfig
+    {
+        public static string host = ConfigurationManager.AppSettings["hostPort"];
+    }
+}
---
+++ 远程控制/Form1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 远程控制
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            StartHost();  //
+        }
+
+        public void StartHost() {
+            using (WebApp.Start<Startup>(AppConfig.host))
+            {
+                Console.WriteLine("server running...");
+                Console.ReadLine();
+            }
+        }
+    }
+
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ;
+        }
+    }
+}
